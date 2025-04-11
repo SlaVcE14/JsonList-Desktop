68a68cfd398bfa2152da4f69d3366abbd5cb990d
SlaVcE <slavce14.apps@gmail.com>
2025-04-11 03:58:27 +0200

    Create ListItemController.java

---
+++ src/main/java/com/sj14apps/jsonlist/ListItemController.java
@@ -0,0 +1,14 @@
+package com.sj14apps.jsonlist;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ListItemController {
+    @FXML
+    public Label itemName;
+    @FXML
+    public Label itemValue;
+    @FXML
+    public VBox itemVB;
+}
